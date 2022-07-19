d0c9b28a1943996c708af5e69987c250f4e4dea5
Aayush Jha <80845650+aayushjha3135@users.noreply.github.com>
2022-07-19 20:07:22 +0530

    Add files via upload

---
+++ Face Recognition Opencv/Face_compare.py
@@ -0,0 +1,19 @@
+import cv2
+import face_recognition
+
+#face detection
+#comparing photos in the given database
+img = cv2.imread("images/messi.jpg")
+rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+img_encoding = face_recognition.face_encodings(rgb_img)[0]
+
+img2 = cv2.imread("images/messi1.jpg")
+rgb_img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
+img_encoding2 = face_recognition.face_encodings(rgb_img2)[0]
+
+result = face_recognition.compare_faces([img_encoding], img_encoding2)
+print("Result: ", result)
+
+cv2.imshow("Img", img)
+cv2.imshow("Img 2", img2)
+cv2.waitKey(0)
